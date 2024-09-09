28160f24593aff7c2146b22cbd93f005a51a6649
DanielKecmanUofT <dkecman@goodlabs.studio>
2024-09-09 14:10:02 -0400

    Updated unit tests
    Added unit tests for type of value num_nodes, missing edges in input dict, incorrect type of edge dictionary and incorrent type of input file

--- tests/test_input.py
+++ tests/test_input.py
@@ -22,6 +22,26 @@ class InputTest(unittest.TestCase):
         with self.assertRaisesRegex(ValueError, "Couldn't find key for number of nodes"):
             num_nodes, edges = get_num_nodes_and_edges_from_dictionary(input)
 
+    def test_bad_input_two(self):
+        input = self._get_bad_input_two()
+        with self.assertRaisesRegex(ValueError, "Number of nodes should be an integer, but given <class 'str'>"):
+            num_nodes, edges = get_num_nodes_and_edges_from_dictionary(input)
+
+    def test_bad_input_three(self):
+        input = self._get_bad_input_three()
+        with self.assertRaisesRegex(ValueError, "Couldn't find key for edges"):
+            num_nodes, edges = get_num_nodes_and_edges_from_dictionary(input)
+
+    def test_bad_input_four(self):
+        input = self._get_bad_input_four()
+        with self.assertRaisesRegex(ValueError, "Dictionary expected for edges, but got <class 'dict'>"):
+            num_nodes, edges = get_num_nodes_and_edges_from_dictionary(input)
+
+    def test_bad_input_five(self):
+        input = self._get_bad_input_five()
+        with self.assertRaisesRegex(ValueError, "Dictionary expected, but got <class 'list'>"):
+            num_nodes, edges = get_num_nodes_and_edges_from_dictionary(input)
+
     def _get_bad_input_one(self):
         return {
             "edges": {
@@ -41,6 +61,30 @@ class InputTest(unittest.TestCase):
                 }
             }
         }
+
+    def _get_bad_input_two(self):
+        return {
+            "num_nodes": "two",
+            "edges": {
+                "0": {
+                    "1": 1
+                }
+            }
+        }
+
+    def _get_bad_input_three(self):
+        return {
+            "num_nodes": 3
+        }
+
+    def _get_bad_input_four(self):
+        return {
+            "num_nodes": 3,
+            "edges": [(0, 1, 1), (0, 2, 1), (1, 2, 1)]
+        }
+
+    def _get_bad_input_five(self):
+        return [(0, 1, 1), (0, 2, 1), (1, 2, 1)]
 
     def _get_good_test_dictionary(self):
         return {
